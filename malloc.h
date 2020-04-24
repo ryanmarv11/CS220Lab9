e43f3e01d8b2dba15fd8a18cb31e7b57cbdddd9f
Ryan Marvin <rmarvin1@binghamton.edu>
2020-04-24 00:13:56 -0400

    Segfault for some reason

--- malloc.h
+++ malloc.h
@@ -1,8 +1,3 @@
-#define malloc(size) mymalloc(size)
-#define free(ptr) myfree(ptr)
-
-//void *mymalloc(size_t size);
-//void myfree(void *ptr);
-void *__wrap_malloc(size_t size);
-void __wrap_free(void *ptr);
+void *malloc(size_t size);
+void free(void *ptr);
 
--- mymalloc.c
+++ mymalloc.c
@@ -1,34 +1,41 @@
-#ifdef LINKTIME
+#ifdef RUNTIME
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
 
+void *malloc(size_t size)
+{
+	void*(*mallocp)(size_t size);
+	char *error;
 
-/* malloc wrapper function
-vvoid *mymalloc(size_t size)
-{
-	void *ptr = malloc(size);
-	printf("malloc(%d)=%p\n", (int)size, ptr);
-	return ptr;
-}
-free wrapper function 
-void myfree(void *ptr)
-{
-	free(ptr);
-	printf("free(%p)\n", ptr);
-}*/
-void *__real_malloc(size_t size);
-void __real_free(void *ptr);
-
-//malloc wrapper function
-void *__wrap_malloc(size_t size)
-{
-	void *ptr = __real_malloc(size); //Call libc malloc
+	mallocp = dlsym(RTLD_NEXT, "malloc");
+	if((error = dlerror()) != NULL)
+	{
+		fputs(error, stderr);
+		exit(1);
+	}
+	char *ptr = mallocp(size);
 	printf("malloc(%d) = %p\n", (int)size, ptr);
 	return ptr;
 }
-//free wrapper function
-void __wrap_free(void *ptr)
+
+void free(void *ptr)
 {
-	__real_free(ptr);
-	printf("free(%p)\n", ptr);
+	void(*freep)(void *) = NULL;
+	char *error;
+	
+	if(!ptr)
+		return;
+
+	freep = dlsym(RTLD_NEXT, "free");
+	if((error = dlerror()) != NULL)
+	{
+		fputs(error, stderr);
+		exit(1);
+	}
+	freep(ptr);
+	printf("free(%p)\n",ptr);
 }
 #endif
+
