21f7b316c8c2e42fdc2eceae27963056ad1a0603
Ryan Marvin <rmarvin1@binghamton.edu>
2020-04-23 23:43:13 -0400

    LINKTIME code

--- malloc.h
+++ malloc.h
@@ -1,6 +1,8 @@
 #define malloc(size) mymalloc(size)
 #define free(ptr) myfree(ptr)
 
-void *mymalloc(size_t size);
-void myfree(void *ptr);
+//void *mymalloc(size_t size);
+//void myfree(void *ptr);
+void *__wrap_malloc(size_t size);
+void __wrap_free(void *ptr);
 
--- mymalloc.c
+++ mymalloc.c
@@ -1,18 +1,34 @@
-#ifdef COMPILETIME
+#ifdef LINKTIME
 #include <stdio.h>
-#include <malloc.h>
 
-/* malloc wrapper function */
-void *mymalloc(size_t size)
+
+/* malloc wrapper function
+vvoid *mymalloc(size_t size)
 {
 	void *ptr = malloc(size);
 	printf("malloc(%d)=%p\n", (int)size, ptr);
+	return ptr;
 }
-
-/*free wrapper function */
+free wrapper function 
 void myfree(void *ptr)
 {
 	free(ptr);
+	printf("free(%p)\n", ptr);
+}*/
+void *__real_malloc(size_t size);
+void __real_free(void *ptr);
+
+//malloc wrapper function
+void *__wrap_malloc(size_t size)
+{
+	void *ptr = __real_malloc(size); //Call libc malloc
+	printf("malloc(%d) = %p\n", (int)size, ptr);
+	return ptr;
+}
+//free wrapper function
+void __wrap_free(void *ptr)
+{
+	__real_free(ptr);
 	printf("free(%p)\n", ptr);
 }
 #endif
